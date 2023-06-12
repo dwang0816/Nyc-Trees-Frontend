2adb1a18e5f3a61317ec242e0bf66dd8f0d5e520
dwang0816 <48970587+dwang0816@users.noreply.github.com>
2023-06-12 11:37:36 -0400

    removed limit also cheers! project now deployed on netlify

--- src/MapBox.js
+++ src/MapBox.js
@@ -44,7 +44,7 @@ class MapBox extends React.Component {
   // }
 
   componentDidMount(){
-    fetch(`https://data.cityofnewyork.us/resource/uvpi-gqnh.json?$limit=3000`)
+    fetch(`https://data.cityofnewyork.us/resource/uvpi-gqnh.json`)
     .then(res => res.json())
     .then(trees => this.setState({treesCollection: trees}))
   }
